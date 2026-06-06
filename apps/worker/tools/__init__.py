f0c4f95e37a1f97a52563dbfba7b8aeb3e789f93
ahubler01 <armand.hubler@gmail.com>
2026-06-07 00:29:19 +0200

    feat: add dataset tools and ingestion script for HK open data

--- apps/worker/tools/__init__.py
+++ apps/worker/tools/__init__.py
@@ -1,7 +1,8 @@
 from context import RunContext
+from tools.datasets import dataset_tools
 from tools.db import db_tools
 from tools.reports import report_tools
 
 
 def build_tools(ctx: RunContext) -> list:
-    return [*db_tools(ctx), *report_tools(ctx)]
+    return [*db_tools(ctx), *report_tools(ctx), *dataset_tools(ctx)]
